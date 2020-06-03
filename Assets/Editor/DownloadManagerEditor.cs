670ef06cd9f0289ae40260ba500bd4bdebb66104
gaohongquan <gao.hongquan@qq.com>
2020-06-03 11:23:51 +0800

    更改BUG

--- Assets/Editor/DownloadManagerEditor.cs
+++ Assets/Editor/DownloadManagerEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Yunchang.Download;
 
 [CustomEditor(typeof(DownloadAsyncOperation))]
 public class DownloadManagerEditor : Editor
